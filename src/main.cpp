04bf93d879eb001fca09ab4e679274397efb8458
nikolyanikolya <nickigna610@gmail.com>
2021-02-18 17:08:32 +0300

    breaking problems with platform

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
+    button.resize(100, 200);
     button.show();
     return QApplication::exec();
 }
