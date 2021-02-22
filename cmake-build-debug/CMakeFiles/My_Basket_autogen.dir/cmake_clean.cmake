60457866fc565582a688633a7f39abf475f07b67
TurovV <70708359+TurovV@users.noreply.github.com>
2021-02-23 01:17:29 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/My_Basket_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/My_Basket_autogen"
-  "My_Basket_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/My_Basket_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
