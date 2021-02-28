9ff89da9469e5cb4e42d4cb910238e905fd63968
nikolyanikolya <nickigna610@gmail.com>
2021-03-01 02:32:57 +0300

    add functions to Zhenya and Vova

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
-#include <search_engine.h>
 #include <QApplication>
 #include <QPushButton>
+#include <work_with_string.h>
 #include <iostream>
-#include "work_with_string.h"
-
-int min(int a, int b) {
-    return (a < b) ? a : b;
-}
+#include <search_engine.h>
+#include <QtGui>
+#include <recipebook.h>
+std::multiset<search::set_unit, search::comp>search::ingredients_to_recipe::res_of_request;
+std::vector<search::product> search::ingredients_to_recipe::chosen_ingredients;
+std::vector<search::Recipe> search::ingredients_to_recipe::recommended_recipes;
+std::vector<search::Recipe> search::recipe_to_ingredients::recipes_request;
+search::Recipe search::recipe_to_ingredients::chosen_recipe;
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(100, 200);
-    button.show();
+    QApplication app(argc, argv);
+    std::string s = "Вода";
+    search::get_prod_top_by_name(s, 10);
+    for(auto t:search::ingredients_to_recipe::show_res_of_request()){
+        std::cout<< t.product_;
+    }
+    RecipeBook recipebook;
+    recipebook.show();
+    return app.exec();
 
-    std::string s = "Яблоки";
-    std::vector<search::product> vec;
-    search::get_prod_top_by_name(s, 10, vec);
-
-    return QApplication::exec();
 }
