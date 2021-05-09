d0c68f4a826fa6e152c2a44b0e5093370a08b60d
diffitask <ginger.ll@mail.ru>
2021-05-10 00:49:18 +0300

    added picture as background

--- src/startwindow.cpp
+++ src/startwindow.cpp
@@ -1,13 +1,29 @@
 #include "startwindow.h"
 #include <QGridLayout>
+#include <QSizePolicy>
+#include <QLabel>
 
 StartWindow::StartWindow(QWidget *parent) : QWidget(parent) {
     start_button = new QPushButton(tr("start shopping!"));
     start_button->show();
 
-    QGridLayout *main_layout = new QGridLayout;
-    main_layout->addWidget(start_button);
+    //добавление картинки
+//    QLabel *image_basket_label = new QLabel;
+//    QPixmap image_basket_pixmap("../data/image_basket.jpeg");
+//    image_basket_pixmap.scaled(50, 50);
+//    image_basket_label->setPixmap(image_basket_pixmap);
+//    image_basket_label->setScaledContents(true);
 
+    QBrush br(QImage("../data/image_basket.jpeg"));
+    QPalette plt = this->palette();
+    plt.setBrush(QPalette::Background, br);
+    this->setPalette(plt);
+
+    QHBoxLayout *main_layout = new QHBoxLayout;
+    main_layout->addWidget(start_button);
+    //main_layout->addWidget(image_basket_label);
+
+    //переход к recipe_book
     recipe_book = new RecipeBook();
     connect(start_button, SIGNAL(clicked()), this, SLOT(go_to_recipe_book()));
 
