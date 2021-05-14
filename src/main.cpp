91d97e2647d60eca3522a080b5f134557b793a51
nikolyanikolya <nickigna610@gmail.com>
2021-05-15 01:31:15 +0300

    version with new design and new ub

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,31 @@
-#include <recipebook.h>
-#include <search_engine.h>
 #include <QApplication>
 #include <QtGui>
 #include <iostream>
 #include "algo.h"
 #include "for_static.h"
+#include "recipebook.h"
+#include "search_engine.h"
+#include "startwindow.h"
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     std::string s = "Вода";
     std::vector<search::product> vec;
     functions::ingredients_to_recipe::run_product_search(s, 10, vec);
     auto top_product = functions::ingredients_to_recipe::show_res_of_request();
-    for (const auto& t : top_product) {
+    for (const auto &t : top_product) {
         std::cout << t;
     }
-    std::cout<<std::endl;
     std::vector<search::Recipe> vec2;
-    std::vector<search::product> ingredients;
-    search::product p("Вода", "Вода", 24);
-    ingredients.push_back(p);
-    functions::ingredients_to_recipe::run_recipes_search(ingredients, 10, vec2);
+    functions::ingredients_to_recipe::run_recipes_search(top_product, 10, vec2);
     auto top_recipes = functions::ingredients_to_recipe::show_recipes();
-    for(const auto& t: top_recipes){
-        std::cout <<t;
+    for (const auto &t : top_recipes) {
+        // std::cout <<t;
     }
-    std::cout<<std::endl;
-    RecipeBook recipebook;
-    recipebook.show();
+
+    // RecipeBook recipebook;
+    // recipebook.show();
+
+    StartWindow start_window;
+    start_window.show();
     return app.exec();
 }
