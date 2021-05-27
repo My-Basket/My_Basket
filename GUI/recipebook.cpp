6187512f82035a7d628c566fad2998b1cb1cc44d
diffitask <ginger.ll@mail.ru>
2021-05-28 00:55:47 +0300

    super cool working version!!!)

--- GUI/recipebook.cpp
+++ GUI/recipebook.cpp
@@ -184,11 +184,11 @@ RecipeBook::RecipeBook(QWidget *parent) : QWidget(parent) {
     setLayout(main_layout);
     setWindowTitle(StyleSettings::Titles::windows_title.c_str());
 
-    //QBrush image_basket_background(
-      //  QImage(StyleSettings::Titles::path_to_bg_image.c_str()));
-   // QPalette plt = this->palette();
-    //plt.setBrush(QPalette::Window, image_basket_background);
-    //this->setPalette(plt);
+    QBrush image_basket_background(
+        QImage(StyleSettings::Titles::path_to_bg_image.c_str()));
+    QPalette plt = this->palette();
+    plt.setBrush(QPalette::Window, image_basket_background);
+    this->setPalette(plt);
 
     this->setMinimumSize(StyleSettings::WindowSizes::min_width_window,
                          StyleSettings::WindowSizes::min_height_window);
@@ -515,11 +515,11 @@ void RecipeBook::choose_recipe_func() {
 }
 
 SummaryWindow::SummaryWindow(QWidget *parent) : QWidget(parent) {
-    //QBrush image_basket_background(
-        //QImage(StyleSettings::Titles::path_to_bg_image.c_str()));
-    //QPalette plt = this->palette();
-    //plt.setBrush(QPalette::Window, image_basket_background);
-    //this->setPalette(plt);
+    QBrush image_basket_background(
+        QImage(StyleSettings::Titles::path_to_bg_image.c_str()));
+    QPalette plt = this->palette();
+    plt.setBrush(QPalette::Window, image_basket_background);
+    this->setPalette(plt);
 
     //получение информации о лучшем магазине и лучшей стоимости
     auto calculation_info =
